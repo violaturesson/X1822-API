d970df1fbbe7484b6906453e459903e55bef84c3
Viola Turesson <violaturesson@gmail.com>
2018-02-02 20:54:28 +0100

    upd routes UPDATE, READ

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -16,27 +16,51 @@ module.exports=function(app, db){
                 res.send(result.ops[0]);
             }
         });
-        console.log(req.body);
+        console.log('Added a note');
        // res.send('Hello fron Post');
     });
 
     //read
-    app.get('/notes/:id',cors(), (req, res)=>{
+    app.get('/notes/:id', cors(), (req,res) => {
+        const myDB = db.db('notesdb');
 
-        const myDB=db.db('notesdb1');
-        const id=req.params.id;
-
-        const details={'_id': new objectId(id)};
-
-      
-        myDB.collection('notes').findOne(details, (err, item)=>{
-            if(err){
-                res.send({'error':'An error again...'});
+        const id = req.params.id;
+        const details = {'_id': new objectId(id)};
+        myDB.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
             }
-            else{
+            else {
                 res.send(item);
             }
         });
+        console.log('asking for a note');
+        //res.send('this should be a returned note');
+    })
+
+    app.get('/notes',cors(), (req, res)=>{
+
+        const myDB=db.db('notesdb1');
+        //const id=req.params.id;
+
+        //const details={'_id': new objectId(id)};
+        myDB.collection('notes').find({}).toArray((err, item)=>{
+             if(err){
+                 res.send({'error':'An error again...'});
+             }
+             else{
+                 res.send(item);
+             }
+         });
+      
+        // myDB.collection('notes').findOne(details, (err, item)=>{
+        //     if(err){
+        //         res.send({'error':'An error again...'});
+        //     }
+        //     else{
+        //         res.send(item);
+        //     }
+        // });
 
         console.log('asking for a note');
        //res.send('this should be a returned note');
@@ -51,12 +75,12 @@ module.exports=function(app, db){
         const details={'_id': new objectId(id)};
         const note={text: req.body.body, title: req.body.title};
       
-        myDB.collection('notes').findOne(details,  note, (err, item)=>{
+        myDB.collection('notes').update(details,  note, (err, result)=>{
             if(err){
                 res.send({'error':'An error again...'});
             }
             else{
-                res.send(item);
+                res.send(note);
             }
         });
 
