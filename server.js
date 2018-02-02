5041d139bfe55c7c18c67e569014cfc7ff2788c0
Viola Turesson <violaturesson@gmail.com>
2018-02-02 17:08:08 +0100

    upd server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser=require('body-parser');
 const db=require('./config/db');
 const mongoclient=require('mongodb').MongoClient;
+const cors = require('cors');
 
 const app =express();
 const port=8088;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 
 mongoclient.connect(db.url, (err, database)=>{
